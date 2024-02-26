2c2ce42a262a91de949543c874bf3672326fc1c8
Iokete <jorgee.mc@hotmail.com>
2024-02-26 12:01:25 +0100

    Created CstErrorController.java to handle the status errors and show custom error pages. TODO: check return error/x (?) and customize more the error pages. + modified app.properties to serve the errors. Saved error html pages into templates/error/

---
+++ src/main/java/com/example/webdws/controller/CstErrorController.java
@@ -0,0 +1,41 @@
+package com.example.webdws.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/*
+*   This class handles the specific error the website comes across, and shows a different page depending on its status code.
+*
+*   Class : CstErrorController
+*   Implements : ErrorController
+*       Function : handleError
+*       Param : request
+*       return : specific error page for the status code resolved.
+*
+ */
+
+@Controller
+public class CstErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest req){
+        Object status = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null){
+            Integer status_code = Integer.valueOf(status.toString());
+
+            if (status_code == HttpStatus.NOT_FOUND.value()){
+                return "error/404";
+            }
+            else if (status_code == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                return "error/500";
+            }
+        }
+
+        return "error/error";
+    }
+}
